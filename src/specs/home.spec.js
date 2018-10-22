c1fcc3df58f04f36459bccabc62c42ef3de04825
chinu-cho <chinu.cho@bizreach.co.jp>
2018-10-22 15:36:33 +0900

    config prettier

--- src/specs/home.spec.js
+++ src/specs/home.spec.js
@@ -2,25 +2,27 @@ const puppeteer = require('puppeteer');
 const expect = require('chai').expect;
 let browser, page;
 
-beforeScenario(async() => {
+beforeScenario(async () => {
   browser = await puppeteer.launch();
   page = await browser.newPage();
 });
 
-afterScenario(async() => await browser.close());
+afterScenario(async () => await browser.close());
 
-step("Open the home page", async function() {
-	await page.goto(`http://localhost:${process.env.NODE_ENV === 'dev' ? 8000 : 9000}/`);
+step('Open the home page', async function() {
+  await page.goto(
+    `http://localhost:${process.env.NODE_ENV === 'prod' ? 9000 : 8000}/`,
+  );
 });
 
-step("<location> should show up in the home page", async(location) => {
+step('<location> should show up in the home page', async location => {
   await page.waitFor('body');
   const body = await page.$eval('body', e => e.innerText);
   expect(body).to.have.string(location);
 });
 
-step("<word> should not show up in the home page", async word => {
-	await page.waitFor('body');
+step('<word> should not show up in the home page', async word => {
+  await page.waitFor('body');
   const body = await page.$eval('body', e => e.innerText);
   expect(body).to.not.have.string(word);
 });
