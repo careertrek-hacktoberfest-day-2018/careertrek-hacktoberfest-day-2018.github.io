3d863c82c8ad2cee63e523e3a32551df7a01c6af
cho0o0 <choqu@outlook.com>
2018-10-25 00:47:55 +0900

    fix setup-github.sh

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 # https://jtway.co/deploying-jekyll-to-github-pages-with-circleci-2-0-3eb69324bc6e
+ssh -o "StrictHostKeyChecking=no"
 
 git config user.name "$GH_NAME"
 git config user.email "$GH_EMAIL"
 
 git checkout master
+echo "pulling master branch"
 git pull origin master
 
+echo "reorder files"
 find . -maxdepth 1 ! -name 'public' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv public/* .
 rm -R public/
 
+echo "push files to master branch"
 git add -fA
 git commit --allow-empty -m "auto deploy [ci skip]"
 git push -f origin master
