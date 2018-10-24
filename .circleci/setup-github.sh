48325f6146ecd294d088f12b1671d8c8d6e14711
cho0o0 <choqu@outlook.com>
2018-10-25 00:03:44 +0900

    change deploy function to script

---
+++ .circleci/setup-github.sh
@@ -0,0 +1,17 @@
+# https://jtway.co/deploying-jekyll-to-github-pages-with-circleci-2-0-3eb69324bc6e
+
+git config user.name "$GH_NAME"
+git config user.email "$GH_EMAIL"
+
+git checkout master
+git pull origin master
+
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv _site/* .
+rm -R _site/
+
+git add -fA
+git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
+git push origin master
+
+echo "deployed successfully"
